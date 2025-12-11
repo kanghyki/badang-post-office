4b1546b4e0fb8de148c3a16b5679d5f782ec0e1d
kanghyki <kanghyki@gmail.com>
2025-12-11 14:09:33 +0900

    fix: build

--- fe/app/modify/page.tsx
+++ fe/app/modify/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./home.module.scss";
+import styles from "../()/home.module.scss";
 import Link from "next/link";
 export default function Home() {
   return (
