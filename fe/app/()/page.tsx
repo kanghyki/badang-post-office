65104147fef01ddb679bc7e6deed17a9a84d0664
kanghyki <kanghyki@gmail.com>
2025-12-17 16:05:55 +0900

    design: enhance onboarding experience with new styles and background gradients

--- fe/app/()/page.tsx
+++ fe/app/()/page.tsx
@@ -9,23 +9,19 @@ export default function Home() {
   const [targetRoute, setTargetRoute] = useState(ROUTES.LOGIN);
 
   useEffect(() => {
-    document.body.style.backgroundColor = "#FE6716"; // 시작화면 배경색
-
     // 온보딩을 본 적이 없으면 온보딩으로, 봤으면 로그인으로
     const hasSeenOnboarding = localStorage.getItem("hasSeenOnboarding");
     if (!hasSeenOnboarding) {
       setTargetRoute(ROUTES.ONBOARDING);
     }
-
-    return () => {
-      document.body.style.backgroundColor = ""; // 나갈 때 초기화
-    };
   }, []);
 
   return (
     <>
       <main className={styles.homeMain}>
-        <Logo c_value="#FFF" bg_value="#fff" />
+        <div className={styles.logoWrapper}>
+          <Logo c_value="#FFF" bg_value="#fff" />
+        </div>
         <p className={styles.headCopy}>
           시간을 담아 보내는
           <br />
