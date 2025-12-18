9e46df641990b55ea22b2db3f4e3a8e9eaa82f59
kanghyki <kanghyki@gmail.com>
2025-12-18 11:49:38 +0900

    design: revert root page

--- fe/app/()/page.tsx
+++ fe/app/()/page.tsx
@@ -6,26 +6,32 @@ import { ROUTES } from "@/lib/constants/urls";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [targetRoute, setTargetRoute] = useState<string>(ROUTES.LOGIN);
+  const [targetRoute, setTargetRoute] = useState(ROUTES.LOGIN);
 
   useEffect(() => {
+    document.body.style.backgroundColor = "#FE6716"; // 시작화면 배경색
+
     // 온보딩을 본 적이 없으면 온보딩으로, 봤으면 로그인으로
     const hasSeenOnboarding = localStorage.getItem("hasSeenOnboarding");
     if (!hasSeenOnboarding) {
       setTargetRoute(ROUTES.ONBOARDING);
     }
+
+    return () => {
+      document.body.style.backgroundColor = ""; // 나갈 때 초기화
+    };
   }, []);
 
   return (
     <>
       <main className={styles.homeMain}>
-        <div className={styles.logoWrapper}>
-          <Logo c_value="#FFF" bg_value="#fff" />
-        </div>
+        <Logo c_value="#FFF" bg_value="#fff" />
         <p className={styles.headCopy}>
-          작은 마음 담은 엽서 한 통
+          시간을 담아 보내는
           <br />
-          바당우체국이 전해드려요
+          제주방언 느영나영 편지,
+          <br />
+          미래에 도착해서 만나.
         </p>
         <div className={styles.buttonSection}>
           <Link href={targetRoute} className={styles.moveBtn}>
--- fe/app/components/LogoBox.tsx
+++ fe/app/components/LogoBox.tsx
@@ -2,25 +2,30 @@ import Image from "next/image";
 import styles from "./logoBox.module.scss";
 
 interface LogoBoxProps {
-  c_value: string;
-  bg_value: string;
+    c_value: string;
+    bg_value: string;
 }
 
 export default function LogoBox({ c_value, bg_value }: LogoBoxProps) {
-  return (
-    <>
-      <div className={styles.logoBox}>
-        <div className={styles.logoImage}>
-          <Image
-            src="/images/logoImg.png"
-            alt="바당우체국 로고"
-            fill
-            style={{ objectFit: "contain" }}
-            unoptimized
-          />
-        </div>
-        <h1 className={styles.logoTitle} style={{color:c_value}}>바당우체국</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.logoBox}>
+                <div className={styles.logoImage}>
+                    <Image
+                        src="/images/logoImg2.png"
+                        alt="바당우체국 로고"
+                        fill
+                        style={{
+                            objectFit: "contain",
+                            padding: "2rem",
+                        }}
+                        unoptimized
+                    />
+                </div>
+                <h1 className={styles.logoTitle} style={{ color: c_value }}>
+                    바당우체국
+                </h1>
+            </div>
+        </>
+    );
 }
